3e0fd7403f5e01f4f8ea40274e652496eb92176f
Hrushikesh Patel <hrushikesh592002@gmail.com>
2023-07-15 14:40:26 +0530

    first commit

---
+++ src/redux/reducers/reducer.js
@@ -0,0 +1,53 @@
+const INIT_STATE = {
+  carts: [],
+};
+
+export const cartreducer = (state = INIT_STATE, action) => {
+  switch (action.type) {
+    case "ADD_CART":
+    case "ADD_CART":
+      const IteamIndex = state.carts.findIndex(
+        (iteam) => iteam.id === action.payload.id
+      );
+
+      if (IteamIndex >= 0) {
+        state.carts[IteamIndex].qnty += 1;
+        return {
+          ...state,
+          carts: [...state.carts],
+        };
+      } else {
+        const temp = { ...action.payload, qnty: 1 };
+        return {
+          ...state,
+          carts: [...state.carts, temp],
+        };
+      }
+
+    case "RMV_CART":
+      const data = state.carts.filter((el) => {
+        return el.id !== action.payload;
+      });
+      return { ...state, carts: data };
+
+    case "RMV_ONE":
+      const IteamIndex_dec = state.carts.findIndex(
+        (iteam) => iteam.id === action.payload.id
+      );
+      if (state.carts[IteamIndex_dec].qnty > 1) {
+        const dltitem = (state.carts[IteamIndex_dec].qnty -= 1);
+        return {
+          ...state,
+          carts: [...state.carts],
+        };
+      } else {
+        const data = state.carts.filter((el) => {
+          return el.id !== action.payload;
+        });
+        return { ...state, carts: data };
+      }
+
+    default:
+      return state;
+  }
+};
---
+++ src/store.js
@@ -0,0 +1,6 @@
+import { legacy_createStore } from "redux";
+import rootred from "./redux/reducers/main";
+
+const store = legacy_createStore(rootred);
+
+export default store;
